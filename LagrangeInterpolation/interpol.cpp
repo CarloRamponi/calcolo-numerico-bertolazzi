61b89582f57554fbfabf233beef9f8eb96f6b18b
giacorri <giacomo.giacorri@gmail.com>
2021-11-25 16:54:09 +0100

    aggiunti commenti Lagrange

--- LagrangeInterpolation/interpol.cpp
+++ LagrangeInterpolation/interpol.cpp
@@ -12,21 +12,26 @@
 using namespace std;
 using namespace Eigen;
 
+// Retrieve the polynomial passing through (x[i], fx[i]) points
 Poly LagrangeInterpolation(VectorXd x, VectorXd fx){
-    int n=x.size();
+    int n=x.size();     // number of points
     Poly f(n);
     double temp;
+    
     for (int i=0; i<n; i++){
         VectorXd one(1);
-        one(0)=1;
-        Poly fMult(one);
+        one(0)=1;               // Initialize fMult polynomial
+        Poly fMult(one);        // to fMult(x) = 1
+
         VectorXd w(1);
-        w(0) = fx(i);
-        Poly fAdd(w);
+        w(0) = fx(i);           // Initialize fAdd polynomial
+        Poly fAdd(w);           // to fAdd(x) = f(x_i)
+
         for (int j=0; j<n; j++){
-            if(i==j)
+
+            if(i==j)            // Skip when i=j
                 j++;
-            if(j==n)
+            if(j==n)            // Fix if we skip when i=j=n-1
                 break;
 
             VectorXd v = VectorXd::Zero( 2 );
@@ -36,8 +41,11 @@ Poly LagrangeInterpolation(VectorXd x, VectorXd fx){
             Poly fTemp(v);
             fMult = fMult * fTemp;
         }
+
         fAdd = fAdd * fMult;        
         f = f + fAdd;
+
     }
+
     return f;
 }
